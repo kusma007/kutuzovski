87ae88ed2294cd966300371893e33d9cfdf9355f
kusma007 <kusma007@inbox.ru>
2016-07-24 22:31:55 +0200

    add map

--- themes/site/views/site/index.php
+++ themes/site/views/site/index.php
@@ -5,27 +5,120 @@
 $this->title = 'Торговый комплекс Кутузовский';
 ?>
 <div class="site-index">
-    <div class="main-picture-block"><div class="main-picture"></div></div>
+    <div class="main-picture-block">
+<!--        <div class="main-picture"></div>-->
+        <div class="full-page-bg"></div>
+
+    </div>
     <div id="komplex">
-        <div class="jumbotron">
-            <h1>Congratulations!</h1>
-
-            <p class="lead">You have successfully created your Yii-powered application.</p>
-
-            <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-        </div>
+        <h2 class="text-center">О комплексе</h2>
+        <p><b>Мы рады приветсвовать в Торговом комплексе "Кутузовский", в мире ярких идей, впечатлений и эмоций!</b></p>
+        <p>
+            ТК "Кутузовский" откроется  полностью осенью 2016 года. ТЦ "Кутузовский" создан для тех, кто ценит кеординарность,
+            любит делать приятные подарки себе и своим близким, следит за модой и сохраняет свою индивидуальность.
+            Благодаря атмосфере праздника, царящей в торговом комплексе, к нам хочется возвращатся снова и снова
+        </p>
+        <p>
+            Витрины бутиков и разнообразие брендов и ассортимента поразят воображения даже искушенных ценителей моды
+            ТК "Кутузовский" представлены ведущие российские и европейские бренды мужской и женской одежды. аксессуаров,
+            кожгалантереи, обуви, парфюмерии и косметики, бытовой техники и многое другое.
+        </p>
+        <p>
+            В нашем торговом комплексе можно не только процтись по мгазинам, а так же в пельменую, для утоления голода.
+        </p>
+        <p>
+            Для ТК "Кутузовский" была разработана оригинальная визуализальная концепция британской студией.
+        </p>
+        <p>
+            К безусловным преимуществам ТК "Кутузовский" стоит отнести наличие наземного пракинга, зоны отдыха и кафе,
+            что делает комплекс прекрасным местом для достижения необходимых целей, даже всей семьей.
+        </p>
+        <p><b>ТК "Кутузовский" - праздник каждый день!</b></p>
     </div>
     <div id="advantages">
-        <div class="jumbotron">
-            <h1>Congratulations!</h1>
-
-            <p class="lead">You have successfully created your Yii-powered application.</p>
-
-            <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <div class="full-page-bg"></div>
+        <div class="row">
+            <div class="col-xs-4">
+                <div class="row">
+                    <div class="col-xs-4"></div>
+                    <div class="col-xs-8">
+                        <div>3</div>
+                        <div>корпуса</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-4">
+                <div class="row">
+                    <div class="col-xs-4"></div>
+                    <div class="col-xs-8">
+                        <div>1300 м<sup>2</sup></div>
+                        <div>площадь центра</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-4">
+                <div class="row">
+                    <div class="col-xs-4"></div>
+                    <div class="col-xs-8">
+                        <div>24 часа</div>
+                        <div>ваш офис под нашей охраной</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-4">
+                <div class="row">
+                    <div class="col-xs-4"></div>
+                    <div class="col-xs-8">
+                        <div>300</div>
+                        <div>уютных офисов</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-4">
+                <div class="row">
+                    <div class="col-xs-4"></div>
+                    <div class="col-xs-8">
+                        <div>500</div>
+                        <div>парковочных мест</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-4">
+                <div class="row">
+                    <div class="col-xs-4"></div>
+                    <div class="col-xs-8">
+                        <div>15 минут</div>
+                        <div>до центра города</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-
+    <div id="location">
+        <a class="dg-widget-link"
+           href="http://2gis.ru/kaliningrad/firm/70000001018143586/center/20.52683830261231,54.768092615794814/zoom/16?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть
+            на карте Калининграда</a>
+        <div class="dg-widget-link"><a
+                href="http://2gis.ru/kaliningrad/firm/70000001018143586/photos/70000001018143586/center/20.52683830261231,54.768092615794814/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=photos">Фотографии
+                компании</a></div>
+        <div class="dg-widget-link"><a
+                href="http://2gis.ru/kaliningrad/center/20.526833,54.768032/zoom/16/routeTab/rsType/bus/to/20.526833,54.768032╎Балт Маркет Групп, ООО, торговая компания?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти
+                проезд до Балт Маркет Групп, ООО, торговая компания</a></div>
+        <script charset="utf-8" src="http://widgets.2gis.com/js/DGWidgetLoader.js"></script>
+        <script charset="utf-8">new DGWidgetLoader({
+//                "width": 640,
+                "height": 600,
+                "borderColor": "#a3a3a3",
+                "pos": {"lat": 54.768092615794814, "lon": 20.52683830261231, "zoom": 16},
+                "opt": {"city": "kaliningrad"},
+                "org": [{"id": "70000001018143586"}]
+            });</script>
+        <noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript. Включите его в
+            настройках вашего браузера.
+        </noscript>
+    </div>
     <div class="body-content">
 
         <div class="row">
